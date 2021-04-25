81cf8238d06c70dc75207c9ef9f95b85d687e94e
SakshamKJ <72131625+SakshamKJ@users.noreply.github.com>
2021-04-25 23:24:49 +0530

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 
 #define V 4                       /*NUMBER OF VERTICES*/
-// an array of vectors can be used as a matrix by enclosing them in curly braces
-vector<int> djikstra(int graph[V][V],int src) 
+// an array of vectors can be used as a matrix by enclosing them in curly braces and using two square brackets to get the values at different indexes.
+vector<int> djikstra(int graph[V][V],int src)      //V STANDS FOR NUMBER OF VERTICES AND src STANDS FOR SOURCE VERTEX FROM WHICH WE WANT TO FIND THE SHORTEST DISTANCE
 { 
 
-	vector<int> dist(V,INT_MAX);					//dist = distance array
-	dist[src]=0;
-	vector<bool> fin(V,false);					//fin = finalized array
+	vector<int> dist(V,INT_MAX);					//dist = distance array whose size is V and we initialize it as infinite.
+									// we are going to use vertices as indexes in this array!
+	
+	dist[src]=0;							// initialized the distance of source vertex as 0
+	vector<bool> fin(V,false);					//fin = finalized array which is a Boolean Array which is initialized as false and its size is V.
 
 	for (int count = 0; count < V-1 ; count++) 
 	{ 
