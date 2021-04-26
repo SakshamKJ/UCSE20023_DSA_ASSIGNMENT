c33bd12e200ac47625bd984a5382515a3f77ae55
SakshamKJ <72131625+SakshamKJ@users.noreply.github.com>
2021-04-26 07:25:29 +0530

    Update 9.c

--- 9.c
+++ 9.c
@@ -4,27 +4,27 @@ void quicksort (int [], int, int);
  
 int main()
 {
-    int list[40];
-    int size, i;
+    int arr[40];
+    int arr_size, i;
  
     printf("Enter the number of elements: ");
-    scanf("%d", &size); 
+    scanf("%d", &arr_size); 
     printf("Enter the elements to be sorted:\n");
-    for (i=0;i<size;i++)
+    for (i=0;i<arr_size;i++)
     {
-        scanf("%d",&list[i]);
+        scanf("%d",&arr[i]);
     } 
-    quicksort(list,0,size-1);
+    quicksort(arr,0,arr_size-1);
     printf("After applying quick sort\n");
-    for (i=0;i<size;i++)
+    for (i=0;i<arr_size;i++)
     {
-        printf("%d ",list[i]);
+        printf("%d ",arr[i]);
     }
     printf("\n");
  
     return 0;
 }
-void quicksort(int list[], int low, int high)
+void quicksort(int arr[], int low, int high)
 {
     int pivot, i, j, temp;
     if (low < high)
@@ -34,25 +34,25 @@ void quicksort(int list[], int low, int high)
         j = high;
         while (i < j) 
         {
-            while (list[i] <= list[pivot] && i <= high)
+            while (arr[i] <= arr[pivot] && i <= high)
             {
                 i++;
             }
-            while (list[j] > list[pivot] && j >= low)
+            while (arr[j] > arr[pivot] && j >= low)
             {
                 j--;
             }
             if (i < j)
             {
-                temp = list[i];
-                list[i] = list[j];
-                list[j] = temp;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
-        temp = list[j];
-        list[j] = list[pivot];
-        list[pivot] = temp;
-        quicksort(list, low, j - 1);
-        quicksort(list, j + 1, high);
+        temp = arr[j];
+        arr[j] = arr[pivot];
+        arr[pivot] = temp;
+        quicksort(arr, low, j - 1);
+        quicksort(arr, j + 1, high);
     }
 }
